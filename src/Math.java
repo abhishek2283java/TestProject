09e33b7168f6ece6e02f0ed6e39e0d30974678e0
abhishek <abhishek2283@hotmail.com>
2021-06-23 07:47:26 +0200

    added divide method

--- src/Math.java
+++ src/Math.java
@@ -11,4 +11,9 @@ public class Math {
     public int multiply(int a, int b) {
         return a * b;
     }
+
+    public int divide(int a, int b) {
+        //Add later
+        return 0;
+    }
 }
